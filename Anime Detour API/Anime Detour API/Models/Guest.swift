1f335e9c4777025f8af5cc64286109b02a9242af
Brendon Justin <brendonjustin@gmail.com>
2015-01-31 13:23:24 -0600

    Add photos to the Guest model

--- Anime Detour API/Anime Detour API/Models/Guest.swift
+++ Anime Detour API/Anime Detour API/Models/Guest.swift
@@ -8,16 +8,37 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 public class Guest: NSManagedObject {
 
     @NSManaged public var category: String
     @NSManaged public var photoPath: String
     @NSManaged public var hiResPhotoPath: String
+    @NSManaged public var photoData: NSData?
+    @NSManaged public var hiResPhotoData: NSData?
     @NSManaged public var guestID: String
     @NSManaged public var firstName: String
     @NSManaged public var lastName: String
     @NSManaged public var bio: String
+
+    public var hiResPhoto: UIImage? {
+        get {
+            return self.hiResPhotoData.map { UIImage(data: $0) } ?? nil
+        }
+        set {
+            self.hiResPhotoData = newValue.map { UIImageJPEGRepresentation($0, 0.8) }
+        }
+    }
+
+    public var photo: UIImage? {
+        get {
+            return self.photoData.map { UIImage(data: $0) } ?? nil
+        }
+        set {
+            self.photoData = newValue.map { UIImageJPEGRepresentation($0, 0.8) }
+        }
+    }
 
     class public var entityName: String {
         return "Guest"
