dc817e1b56e50666d0c3c146c4464a84b4318af2
Brendon Justin <brendonjustin@gmail.com>
2015-02-07 14:49:03 -0600

    Switch the guest view to a collection view
    Looks hilarious(ly ugly) in iPad.

---
+++ Anime Detour/View Controllers/GuestCollectionViewController.swift
@@ -0,0 +1,141 @@
+//
+//  GuestCollectionViewController.swift
+//  Anime Detour
+//
+//  Created by Brendon Justin on 2/7/15.
+//  Copyright (c) 2015 Anime Detour. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+import AnimeDetourAPI
+
+/**
+Collection view controller displaying `Guest`s. Requires the collection view to use a `UICollectionViewFlowLayout`.
+*/
+class GuestCollectionViewController: UICollectionViewController, CollectionViewFetchedResultsControllerCellCustomizer {
+
+    lazy var imageSession: NSURLSession = NSURLSession.sharedSession()
+
+    /// Detail segue identifier
+    @IBInspectable var detailIdentifier: String!
+
+    /// Cell reuse identifier
+    @IBInspectable var reuseIdentifier: String!
+
+    /// Section header reuse identifier
+    @IBInspectable var headerIdentifier: String!
+
+    /// Lazily created FRC. To use, first perform a fetch on it.
+    private lazy var fetchedResultsController: NSFetchedResultsController = {
+        let moc = self.managedObjectContext
+        let entity = NSEntityDescription.entityForName(Guest.entityName, inManagedObjectContext: moc)
+        let sort = NSSortDescriptor(key: "firstName", ascending: true)
+        let fetchRequest = NSFetchRequest()
+        fetchRequest.entity = entity
+        fetchRequest.sortDescriptors = [sort]
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "category", cacheName: nil)
+        return frc
+    }()
+
+    private lazy var managedObjectContext = CoreDataController.sharedInstance.managedObjectContext!
+
+    private lazy var fetchedResultsControllerDelegate = CollectionViewFetchedResultsControllerDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.fetchedResultsControllerDelegate.collectionView = self.collectionView
+        self.fetchedResultsControllerDelegate.customizer = self
+
+        let frc = self.fetchedResultsController
+        frc.delegate = self.fetchedResultsControllerDelegate
+
+        var error: NSError?
+        if !frc.performFetch(&error) {
+            NSLog("Error fetching guests: \(error!)")
+        }
+
+        self.setFlowLayoutCellSizes(self.collectionView!)
+    }
+
+    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        self.setFlowLayoutCellSizes(self.collectionView!)
+    }
+
+    private func guest(indexPath: NSIndexPath) -> Guest {
+        return self.fetchedResultsController.objectAtIndexPath(indexPath) as Guest
+    }
+
+    /// Update the sizes of our collection view cells based on the view's trait collection.
+    private func setFlowLayoutCellSizes(collectionView: UICollectionView) {
+        let traitCollection = collectionView.traitCollection
+        let viewWidth = collectionView.frame.width
+        let (minWidth, maxWidth): (CGFloat, CGFloat) = (280, 350)
+        let layout = collectionView.collectionViewLayout as UICollectionViewFlowLayout
+        var itemSize = layout.itemSize
+
+        if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.Compact {
+            itemSize.width = viewWidth
+        } else {
+            // Assume regular
+            let cellsPerRow = floor(viewWidth / minWidth)
+            let widthPerCell = floor(viewWidth / cellsPerRow) // ensure cell widths are integral
+            itemSize.width = widthPerCell
+        }
+
+        layout.itemSize = itemSize
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return self.fetchedResultsController.sections?.count ?? 0
+    }
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.fetchedResultsController.sections?[section] as? NSFetchedResultsSectionInfo)?.numberOfObjects ?? 0
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        self.configure(cell, atIndexPath: indexPath)
+        return cell
+    }
+
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: self.headerIdentifier, forIndexPath: indexPath) as TextHeaderCollectionReusableView
+
+        // Assume that `indexPath` is for item 0 in whatever section to which the header belongs
+        let anyGuest = self.fetchedResultsController.objectAtIndexPath(indexPath) as Guest
+        header.titleLabel.text = anyGuest.category
+
+        return header
+    }
+
+    // MARK: - Collection view cell customizer
+
+    func configure(cell: UICollectionViewCell, atIndexPath indexPath: NSIndexPath) {
+        let cell = cell as GuestCollectionViewCell
+
+        let guest = self.guest(indexPath)
+        let viewModel = GuestViewModel(guest: guest, imageSession: self.imageSession)
+        cell.viewModel = viewModel
+    }
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        switch (segue.identifier) {
+        case .Some(self.detailIdentifier):
+            let cell = sender as GuestCollectionViewCell
+            let guestViewModel = cell.viewModel
+            let guestVC = segue.destinationViewController as GuestDetailTableViewController
+            guestVC.guestViewModel = guestViewModel
+        default:
+            fatalError("Unexpected segue encountered.")
+        }
+    }
+
+}
---
+++ Anime Detour/Views/TextHeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  TextHeaderCollectionReusableView.swift
+//  Anime Detour
+//
+//  Created by Brendon Justin on 2/7/15.
+//  Copyright (c) 2015 Anime Detour. All rights reserved.
+//
+
+import UIKit
+
+class TextHeaderCollectionReusableView: UICollectionReusableView {
+    @IBOutlet var titleLabel: UILabel!
+}
