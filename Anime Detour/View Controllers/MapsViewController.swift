fa7096f3bf8b40e978d0f88a3a1db4f98e48a929
Brendon Justin <brendonjustin@gmail.com>
2015-03-07 19:14:53 -0600

    Implement maps page segmented control

--- Anime Detour/View Controllers/MapsViewController.swift
+++ Anime Detour/View Controllers/MapsViewController.swift
@@ -10,30 +10,47 @@ import UIKit
 
 import QuickLook
 
-class MapsViewController: UIViewController {
-    var dataSource: QLPreviewControllerDataSource!
+class MapsViewController: UIViewController, QLPreviewControllerDataSource {
+    private var previewController: QLPreviewController!
+    private var dataSource: QLPreviewControllerDataSource!
+    private let mapFileNames = ["DoubleTree_Floor1_2015", "DoubleTree_Floor2_2015", "DoubleTree_Floor22_2015"]
+    lazy private var mapPaths: [String] = {
+        let mainBundle = NSBundle.mainBundle()
+        let mapPaths = self.mapFileNames.map { (filename: String) -> String in
+            return mainBundle.pathForResource(filename, ofType: "jpg")!
+        }
+
+        return mapPaths
+    }()
+    private var activeMapIndex: Int = 0 {
+        didSet {
+            self.previewController.reloadData()
+        }
+    }
+
+    private var observingPreviewIndex = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mainBundle = NSBundle.mainBundle()
-        let mapFileNames = ["DoubleTree_Floor1_2015", "DoubleTree_Floor2_2015", "DoubleTree_Floor22_2015"]
-        let mapPaths = mapFileNames.map { (filename: String) -> String in
-            return mainBundle.pathForResource(filename, ofType: "jpg")!
-        }
-        
         let dataSource = MapsDataSource(mapFilePaths: mapPaths)
         self.dataSource = dataSource
-        
+
         let previewController = QLPreviewController()
-        previewController.dataSource = self.dataSource
+        previewController.dataSource = self
         previewController.automaticallyAdjustsScrollViewInsets = true
+        self.previewController = previewController
         
         previewController.view.backgroundColor = UIColor.clearColor()
+        previewController.view.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.view.addSubview(previewController.view)
-        let bindings = ["preview" : previewController.view]
+        let bindings = [
+            "preview" : previewController.view,
+            "top" : self.topLayoutGuide,
+            "bottom" : self.bottomLayoutGuide
+        ]
         let hConstraints = NSLayoutConstraint.constraintsWithVisualFormat("|[preview]|", options: .allZeros, metrics: nil, views: bindings)
-        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[preview]|", options: .allZeros, metrics: nil, views: bindings)
+        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[top][preview][bottom]", options: .allZeros, metrics: nil, views: bindings)
         let previewConstraints = hConstraints + vConstraints
         self.view.addConstraints(previewConstraints)
         
@@ -50,5 +67,24 @@ class MapsViewController: UIViewController {
             let dict = GAIDictionaryBuilder.createScreenView().build()
             analytics.send(dict)
         }
+    }
+
+    // MARK: - Segmented Control
+
+    @IBAction func segmentedControlValueChanged(sender: UISegmentedControl?) {
+        self.activeMapIndex = sender?.selectedSegmentIndex ?? 0
+    }
+
+    // MARK: - Preview Controller Data Source
+
+    func numberOfPreviewItemsInPreviewController(controller: QLPreviewController!) -> Int {
+        return 1
+    }
+
+    func previewController(controller: QLPreviewController!, previewItemAtIndex index: Int) -> QLPreviewItem! {
+        // Assume that we only ever want to show one item, at `activeMapIndex`.
+        // Change `activeMapIndex` to change the active item.
+        let item = NSURL(fileURLWithPath: self.mapPaths[self.activeMapIndex])!
+        return item
     }
 }
