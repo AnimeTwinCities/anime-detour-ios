787d232b9d207c67fba24aec95fe44314557e8ae
Brendon Justin <brendonjustin@gmail.com>
2015-11-14 18:59:46 -0600

    Add Aspects pod and target 9.0

--- Podfile
+++ Podfile
@@ -1,8 +1,9 @@
 source 'https://github.com/CocoaPods/Specs.git'
 
-platform :ios, '8.0'
+platform :ios, '9.0'
 use_frameworks!
 
+pod 'Aspects'
 pod 'FXForms', '~> 1.2.0'
 
 post_install do | installer |
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/Aspects.framework"
   install_framework "Pods/FXForms.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/Aspects.framework"
   install_framework "Pods/FXForms.framework"
 fi
