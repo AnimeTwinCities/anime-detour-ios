54ac87b506c6868cabc88b34e851f9cf642d3719
Brendon Justin <brendonjustin@gmail.com>
2016-01-02 17:00:11 -0600

    Show more of guest image when overscrolling

--- Anime Detour/View Controllers/GuestDetailTableViewController.swift
+++ Anime Detour/View Controllers/GuestDetailTableViewController.swift
@@ -10,27 +10,55 @@ import UIKit
 
 class GuestDetailTableViewController: UITableViewController, UIWebViewDelegate, GuestViewModelDelegate {
     var guestViewModel: GuestViewModel!
-
+    
+    private var imageHeaderView: ImageHeaderView!
     private var bioWebViewHeight: CGFloat?
     private var bioWebviewLoadInitiated = false
 
     @IBInspectable var bioIdentifier: String!
     @IBInspectable var nameIdentifier: String!
-    @IBInspectable var photoIdentifier: String!
 
     /// The aspect ratio (width / height) of the photo image view.
     @IBInspectable var photoAspect: CGFloat = 1
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.guestViewModel.delegate = self
+        
+        self.imageHeaderView = self.tableView.tableHeaderView as! ImageHeaderView
+        self.imageHeaderView.imageView.image = self.guestViewModel.hiResPhoto(true, lowResPhotoPlaceholder: true)
+        
+        self.updateHeaderSize()
+    }
+    
+    /// Adjust the frame of the header view to maintain our target aspect ratio.
+    private func updateHeaderSize() {
+        let headerFrame = self.imageHeaderView.frame
+        let newHeight = round(self.view.frame.width / self.photoAspect)
+        let newFrame = CGRect(origin: headerFrame.origin, size: CGSize(width: headerFrame.width, height: newHeight))
+        self.imageHeaderView.frame = newFrame
+    }
+    
+    /// Adjust the top constraint of the image header view's image,
+    /// so more of the image is visible if the user over-scrolls the table view.
+    private func updateHeaderImageTopConstraint() {
+        let verticalOffset = self.tableView.contentOffset.y
+        let verticalInset = self.tableView.contentInset.top
+        
+        let total = verticalOffset + verticalInset
+        
+        // If `verticalOffset` is < 0, i.e. the table view was overscrolled down such that the
+        // top of its content is lower on the screen than it would be if the user were not touching
+        // the screen, then extend the top of the image header view's image view to fill the empty space.
+        if total < 0 {
+            self.imageHeaderView.imageViewTopConstraint.constant = total
+        } else {
+            self.imageHeaderView.imageViewTopConstraint.constant = 0
+        }
     }
 
     private func configure(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
         switch cell.reuseIdentifier {
-        case .Some(self.photoIdentifier):
-            let imageView = cell.contentView.subviews.filter { return $0 is UIImageView }.first as! UIImageView
-            imageView.image = self.guestViewModel.hiResPhoto(true, lowResPhotoPlaceholder: true)
         case .Some(self.nameIdentifier):
             cell.textLabel?.text = self.guestViewModel.name
         case .Some(self.bioIdentifier):
@@ -44,6 +72,12 @@ class GuestDetailTableViewController: UITableViewController, UIWebViewDelegate, 
         default:
             fatalError("Unexpected reuse identifier: \(cell.reuseIdentifier). Expected a match against one of our xIdentifier properties.")
         }
+    }
+    
+    // MARK: - Scroll view delegate
+    
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.updateHeaderImageTopConstraint()
     }
 
     // MARK: - Table view data source
@@ -63,8 +97,6 @@ class GuestDetailTableViewController: UITableViewController, UIWebViewDelegate, 
         }
         
         switch row {
-        case .Photo:
-            return tableView.frame.width / self.photoAspect
         case .Name:
             return 44
         case .Bio:
@@ -114,17 +146,14 @@ class GuestDetailTableViewController: UITableViewController, UIWebViewDelegate, 
  Cases correspond to data expected to be displayed for a given row.
  */
 private enum GuestDetailTableViewCellRow {
-    case Photo
     case Name
     case Bio
     
     init?(row: Int) {
         switch row {
         case 0:
-            self = .Photo
-        case 1:
             self = .Name
-        case 2:
+        case 1:
             self = .Bio
         default:
             return nil
---
+++ Anime Detour/Views/ImageHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ImageHeaderView.swift
+//  Anime Detour
+//
+//  Created by Brendon Justin on 1/2/16.
+//  Copyright © 2016 Anime Detour. All rights reserved.
+//
+
+import UIKit
+
+class ImageHeaderView: UIView {
+    @IBOutlet var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet var imageView: UIImageView!
+}
